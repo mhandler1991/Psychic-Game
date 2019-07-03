2a5f08272fdf17931a130322fe3acdc9aadabb5c
Max <mhandler1991@gmail.com>
2019-07-02 18:30:09 -0700

    working state of game - need to fix win calc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,63 @@
-// * * Game * * 
-  
-funciton startGame() {
+// Game 
 
-    // Variables //
-    var playerWins = 0;
-    var playerLoses = 0;
-    var playerGuessesLeft = 5;
-    var playerGuesses = 0;
-    var letters = [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "x",
-        "y",
-        "z"
-    ];
-    var userInput = event.key.toLocaleLowerCase();
-    var computerChoice = letters[Math.floor(mamth.random()*letters.length)];
+var startGame = function () {
 
-        //Display Starting Board & Messages
-        document.getElementById("startMessage").innerHTML = "You have 5 gueses, lets begin...";
-        document.getElementById("playerWins").innerHTML = 0;
-        document.getElementById("playerLoses").innerHTML = 0;
-        document.getElementById("playerGuessesLeft").innerHTML = 5;
-        document.getElementById("playerGuesses").innerHTML = "";
+    //Display Starting Board & Messages
+    document.getElementById("startMessage").innerHTML = "You have 5 gueses, lets begin...";
+    document.getElementById("playerWins").innerHTML = 0;
+    document.getElementById("playerLoses").innerHTML = 0;
+    document.getElementById("playerGuessesLeft").innerHTML = 5;
+    document.getElementById("playerGuesses").innerHTML = "";
+    document.getElementById("startReset").innerHTML = "Reset";
+
+    // Variables
+var playerWins = 0;
+var playerLoses = 0;
+var playerGuessesLeft = 5;
+var playerGuesses = "";
+var letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "x",
+    "y",
+    "z"
+];
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+
+//Display Computer Choice
+console.log("Computer Choice: " + computerChoice);
+
+
+
+    // Compare Computer to Person & make calculations on Key Press
+    document.onkeyup = function (event) {
+
+        var userInput = event.key.toLowerCase();
+        // Log User Input
+        console.log("User Choice: " + userInput);
 
         // Determine if player guess is a match
-        if (computerChoice = userInput){
+        if (computerChoice == userInput) {
             // + Win
             playerWins++;
             // Print Win Value
@@ -55,8 +67,7 @@ funciton startGame() {
             // Print Guesses Left
             document.getElementById("playerGuessesLeft").innterHTML = playerGuessesLeft;
 
-        }
-        else {
+        } else {
             // - Guesses Left
             playerGuessesLeft--;
             // Print Guesses Left
@@ -67,16 +78,30 @@ funciton startGame() {
             document.getElementById("playerGuesses").innerHTML = playerGuesses;
         }
 
-        if (playerGuessesLeft = 0){
+        if (playerGuessesLeft === 0) {
             // + Loses
-            playerLoses--;
+            playerLoses++;
             // Print Loss 
             document.getElementById("playerLoses").innerHTML = playerLoses;
-        }
-        else {
+            // Reset Player Guesses
+            playerGuessesLeft = 5;
+            // Print Guesses Left
+            document.getElementById("playerGuessesLeft").innterHTML = playerGuessesLeft;
+            // Computer Chooses new Letter
+            var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+            // Clear Player Guesses
+            playerGuesses = "";
+            // Print PlayerGuesses
+            document.getElementById("playerGuesses").innerHTML = playerGuesses;
+
+        } else {
 
         }
+
+    }
 }
+
+
 
 
 // Reset Means: Update Computer Guess, Update Guesses Left, Update Guesses so far
